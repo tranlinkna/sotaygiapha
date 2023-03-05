f519b688559d331655e2a2ecadcc1b6b86fbbfda
naila <naila@superlever.com>
2023-03-05 17:56:16 +0700

    add keywords for SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
         <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1" />
-        <meta name="keywords" content="Sổ Tay Gia Phả" />
+        <meta name="keywords" content="Sổ Tay Gia Phả, Tạo Gia Phả, Sổ Gia Phả, Gia Phả Dòng Tộc, Gia Phả Online" />
         <meta name="author" content="NaKhanhNhu" />
         <link rel="icon" href="/favicon.ico" />
         <title>
